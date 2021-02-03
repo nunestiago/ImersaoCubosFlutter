d419f225628e37f442ec98fc60fc1f504889f43c
Tiago Nunes <ntiagon@gmail.com>
2021-02-03 15:22:34 -0300

    pre dark mode code

--- lib/trivia_screen.dart
+++ lib/trivia_screen.dart
@@ -6,38 +6,95 @@ class TriviaScreen extends StatefulWidget {
 }
 
 class _TriviaScreenState extends State<TriviaScreen> {
+  int answer = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Color(0xff374952),
-          title: Image(
-            image: AssetImage('lib/assets/logo_small.png'),
-          ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Color(0xff374952),
+        title: Image(
+          image: AssetImage('lib/assets/logo_small.png'),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.fromLTRB(32, 56, 32, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'PERGUNTA 1:',
-                    style: TextStyle(fontSize: 16),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(32, 56, 32, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'PERGUNTA 1:',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'O que é Flutter?',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.15),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(1, 3),
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    height: 16,
+                  child: RadioListTile(
+                    value: 1,
+                    groupValue: answer,
+                    title: Text('É um framework da Google.'),
+                    onChanged: (int value) {
+                      setState(() {
+                        answer = value;
+                      });
+                    },
                   ),
-                  Text(
-                    'O que é Flutter?',
-                    style: TextStyle(fontSize: 18),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.15),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(1, 3),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            )
-          ],
-        ));
+                  child: RadioListTile(
+                    value: 2,
+                    groupValue: answer,
+                    title: Text('É um novo Sistema Operacional.'),
+                    onChanged: (int value) {
+                      setState(() {
+                        answer = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
